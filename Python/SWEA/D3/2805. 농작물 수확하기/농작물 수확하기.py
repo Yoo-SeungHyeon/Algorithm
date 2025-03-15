8a4754480d71be2d3732fa8e347b9fa9ddd43be5
Yoo SeungHyeon <115394922+Yoo-SeungHyeon@users.noreply.github.com>
2025-03-15 19:02:10 +0900

    [D3] Title: 농작물 수확하기, Time: 95 ms, Memory: 61,184 KB -BaekjoonHub

---
+++ Python/SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -0,0 +1,20 @@
+T = int(input())
+for test_case in range(1, T+1):
+    # 농장 크기
+    size = int(input())
+    # 농장 가치
+    farm = []
+    for _ in range(size):
+        farm.append(input())
+    # 중심 세팅
+    center = size//2
+    result = 0
+    for i in range(size):
+        if i <= center:
+            for num in farm[i][center-i:center+i+1]:
+                result += int(num)
+        else:
+            j = size-1- i
+            for num in farm[i][center-j:center+j+1]:
+                result += int(num)
+    print(f'#{test_case} {result}')
