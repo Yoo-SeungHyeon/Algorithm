047dabf993ba6e8679c4e418a0dd954daaf759d4
Yoo SeungHyeon <115394922+Yoo-SeungHyeon@users.noreply.github.com>
2025-03-15 19:01:11 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 65 ms, Memory: 55,936 KB -BaekjoonHub

---
+++ Python/SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -0,0 +1,11 @@
+for _ in range(10):
+    input()
+    p=list(map(int,input().split()))
+    i=-1
+    n=0
+    while p[i]:
+        i=(i+1)%8
+        n=n%5+1
+        p[i]-=n
+        if p[i]<=0:p[i]=0
+    print(f'#{_+1} {" ".join(map(str,p[i+1:]+p[:i+1]))}')
