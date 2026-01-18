4cb9cb8c2fd7a5d86520712762ebb59490036122
Yoo SeungHyeon <effort-result@naver.com>
2026-01-19 01:27:43 +0900

    [level 2] Title: 큰 수 만들기, Time: 4.68 ms, Memory: 5.4 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = ""; // 스택 역할을 할 문자열
+    
+    for (int i = 0; i < number.length(); i++) {
+        char current = number[i];
+        
+        while (!answer.empty() && k > 0 && answer.back() < current) {
+            answer.pop_back();
+            k--;
+        }
+        
+        answer.push_back(current);
+    }
+    
+    if (k > 0) {
+        answer = answer.substr(0, answer.length() - k);
+    }
+    
+    return answer;
+}
